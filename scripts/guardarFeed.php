2390ed48b5f866f9d0ccb55d03f5d47e50cc39b8
leningael <leningaelrs@gmail.com>
2022-03-09 05:06:57 -0600

    Script para añadir nuevos feeds

---
+++ scripts/guardarFeed.php
@@ -0,0 +1,21 @@
+<?php
+    include("../config/bd.php");
+    include("guardarNoticias.php");
+
+    $nombreFeed = (isset($_POST['nombreFeed'])) ? $_POST['nombreFeed']:"";
+    $urlFeed = (isset($_POST['urlFeed'])) ? $_POST['urlFeed']:"";
+    $categoriaFeed = (isset($_POST['categoriaFeed'])) ? $_POST['categoriaFeed']:"";
+
+    $sentenciaSQL = $conexion->prepare("INSERT INTO feeds (nombre, url, categoria) VALUES (:nombreFeed, :urlFeed, :categoriaFeed);");
+    $sentenciaSQL->bindParam(':nombreFeed', $nombreFeed);
+    $sentenciaSQL->bindParam(':urlFeed', $urlFeed);
+    $sentenciaSQL->bindParam(':categoriaFeed', $categoriaFeed);
+    $sentenciaSQL->execute();
+
+    $sentenciaSQL = $conexion->prepare("SELECT id FROM feeds WHERE url = :urlFeed");
+    $sentenciaSQL->bindParam(':urlFeed',$urlFeed);
+    $sentenciaSQL->execute();
+    $feed = $sentenciaSQL->fetch(PDO::FETCH_LAZY);
+    echo $feed['id'];
+    guardarNoticias($feed['id'], generarFeed($urlFeed));
+?>
