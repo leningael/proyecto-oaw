8860fa0794166d927cb51fbbcd2b51c42e5af252
leningael <leningaelrs@gmail.com>
2022-04-06 03:04:37 -0500

    Optimizacion finalizada del lado del cliente

--- proyecto-optimizado/js/buscarNoticia.js
+++
@@ -1,1 +0,0 @@
-var searchBar=document.getElementById("searchBar");searchBar.addEventListener("input",function(){for(var e=searchBar.value.toLowerCase(),b=document.getElementsByClassName("card-news"),c=document.getElementsByClassName("card-title"),a=0;a<c.length;a++){var d=c[a].innerHTML.toLowerCase();console.log(d);d.includes(e)?b[a].style.display="inline-block":b[a].style.display="none"}});
---
+++ sitio-optimizado/config/bd.php
@@ -0,0 +1,1 @@
+<?php $host="localhost";$bd="lector_rss";$usuario="root";$contrasenia="";try{$conexion=new PDO("mysql:host=$host;dbname=$bd",$usuario,$contrasenia);if($conexion){}}catch(Exception $ex){echo $ex->getMessage();} ?>
